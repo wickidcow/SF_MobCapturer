018cf776ba9d252a086c832fcf0d554d25077809
wickidcow <jaywago@gmail.com>
2026-08-23 14:52:03 -0400

    build: prepare 1.0.1 Folia compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,17 +33,15 @@ dependencies {
         compileOnly("com.github.slimefunguguproject:Slimefun4:5c188a3c0a")
     }
 
-    compileOnly("net.guizhanss:GuizhanLibPlugin:2.5.0")
-
     compileOnly("org.projectlombok:lombok:1.18.44")
     annotationProcessor("org.projectlombok:lombok:1.18.44")
 
-    implementation("org.bstats:bstats-bukkit:3.0.3")
+    implementation("org.bstats:bstats-bukkit:3.1.0")
     implementation("com.google.code.findbugs:jsr305:3.0.2")
 }
 
 group = "io.github.wickidcow"
-version = "1.0.0"
+version = "1.0.1"
 description = "MobCapturer for Slimefun Legacy"
 
 java {
@@ -72,7 +70,6 @@ tasks.shadowJar {
 
     doRelocate("org.bstats")
     doRelocate("javax.annotation")
-    doRelocate("io.papermc.paperlib")
 
     archiveClassifier.set("")
     archiveFileName.set("SF_MobCapturer${project.version}.jar")
@@ -91,9 +88,10 @@ publishing {
 bukkit {
     main = "io.github.thebusybiscuit.mobcapturer.MobCapturer"
     apiVersion = "1.21"
+    foliaSupported = true
     authors = listOf("TheBusyBiscuit", "ybw0014", "wickidcow")
     description = "A Slimefun Legacy addon that adds reusable mob capture tools"
     website = "https://github.com/wickidcow/SF_MobCapturer"
     depend = listOf("Slimefun")
-    softDepend = listOf("GuizhanLibPlugin")
+    softDepend = listOf("EliteMobs", "MythicMobs")
 }
