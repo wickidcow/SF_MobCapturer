247af3abab6ddfb1db9b40fb664207a99da7d33a
wickidcow <jaywago@gmail.com>
2026-08-23 14:59:20 -0400

    refactor: internalize axolotl display helper

---
+++ src/main/java/net/guizhanss/minecraft/guizhanlib/gugu/minecraft/helpers/entity/AxolotlHelper.java
@@ -0,0 +1,8 @@
+package net.guizhanss.minecraft.guizhanlib.gugu.minecraft.helpers.entity;
+
+public final class AxolotlHelper {
+    private AxolotlHelper() {}
+    public static String getVariantName(String value) {
+        return DisplayNameHelper.humanize(value);
+    }
+}
